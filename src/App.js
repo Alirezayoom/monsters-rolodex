51b9773d57fa46fe16afb85133e215dfe3c8f36f
alirezayoom <alirezayoom@gmail.com>
2023-02-06 09:40:14 +0430

    reset

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { useState } from "react";
-import MoviesList from "./components/MoviesList";
+import "./App.css";
 
-const App = () => {
-  const [movies, setMovies] = useState([]);
-
-  const fetchData = async () => {
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-
-    const fetchedData = data.results.map((result) => {
-      return {
-        id: result.episode_id,
-        title: result.title,
-        relaseDate: result.release_date,
-        openingText: result.opening_crawl,
-      };
-    });
-    setMovies(fetchedData);
-  };
-
-  return (
-    <div>
-      <button onClick={fetchData}>fetch data</button>
-      <MoviesList movies={movies} />
-    </div>
-  );
-};
+function App() {
+  return <div>hi</div>;
+}
 
 export default App;

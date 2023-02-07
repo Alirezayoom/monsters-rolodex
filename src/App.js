90eae4230b83d8591ec5a9f3db2d8c01d9995eb3
alirezayoom <alirezayoom@gmail.com>
2023-02-07 08:56:18 +0430

    three individual monsters added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monster1: { name: "Linda" },
+      monster2: { name: "Frank" },
+      monster3: { name: "Jacky" },
+    };
+  }
+
   render() {
-    return <div>hi</div>;
+    return (
+      <div>
+        <h1>{this.state.monster1.name}</h1>
+        <h1>{this.state.monster2.name}</h1>
+        <h1>{this.state.monster3.name}</h1>
+      </div>
+    );
   }
 }
 

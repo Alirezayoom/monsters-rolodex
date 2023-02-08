253acd4169f75f9f8f4a922c5999add52f2e6e28
alirezayoom <alirezayoom@gmail.com>
2023-02-08 10:51:08 +0430

    font and title added

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
 
     return (
       <div>
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           className="monsters-search-box"
           placeholder="search monsters"
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,7 +6,7 @@ class Card extends Component {
   render() {
     const { id, name, email } = this.props.monster;
     return (
-      <div className="card-container" key={id}>
+      <div key={id} className="card-container">
         <img
           src={`https://robohash.org/${id}?set=set2&size=180x180`}
           alt={`monster ${name}`}

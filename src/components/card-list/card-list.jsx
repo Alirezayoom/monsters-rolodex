f819f9af5aca2cc792c1b3a30e0020cfeb7701a8
alirezayoom <alirezayoom@gmail.com>
2023-02-09 14:54:10 +0430

    card-list converted to functional comoponent

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -1,19 +1,16 @@
-import { Component } from "react";
 import Card from "../card/card";
 
 import "./card-list.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = (props) => {
+  const { monsters } = props;
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
